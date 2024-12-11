ce504fdfd4d30430833bc3837beaa158cf180da3
John Arban Lewis <johnarban@users.noreply.github.com>
2024-12-11 18:45:43 -0500

    update subset to work as a function

--- subset_files.sh
+++ subset_files.sh
@@ -8,8 +8,17 @@ outputDir=$inputDir"/subsetted_netcdf"
 
 mkdir -p "$outputDir"  # Create the output directory if it doesn't exist
 
+# the user may pass a list of files, check for that. if there are none use the ls
+if [ $# -eq 1 ]; then
+  echo "No files passed, using all files in the input directory"
+  files=($(ls $inputDir/*.nc))
+else
+  files=("$@")
+  # remove the first argument which is the input directory
+  files=("${files[@]:1}")
+fi
 # Get the list of *.nc files in the input directory
-files=($(ls $inputDir/*.nc))
+
 
 # Count the total number of *.nc files in the input directory using wc -l
 totalFiles=$(ls $inputDir/*.nc | wc -l)
@@ -22,7 +31,7 @@ for file in "${files[@]}"; do
 
   # Use subset_tempo_data.py to subset the file and save it in the output directory
   basename="${file##*/}"
-  ./subset_tempo_data.py -f $inputDir/$basename -o $outputDir -d
+  ./subset_tempo_data.py -f $inputDir/$basename -o $outputDir -d -n
 
   # Clear the progress bar after updating it
   # printf "\033[2K\r"
--- subset_tempo_data.py
+++ subset_tempo_data.py
@@ -2,7 +2,11 @@
 from pathlib import Path
 import argparse, sys
 import netCDF4 as nc
+from netCDF4 import Dataset # type: ignore
 
+from logger import setup_logging, set_log_level
+
+logger = setup_logging()
 
 class Timer:
     # via github copilot
@@ -32,111 +36,59 @@ class Timer:
     def stop(self):
         self.stop_timer = True
 
+class TimedContext(object):
+    def __init__(self, use_timer = True):
+        self.timer = Timer()
+        self.use_timer = use_timer
+    
+    def __enter__(self):
+        if self.use_timer:
+            self.timer.start()
+        return self.timer
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.timer.stop()
 
-parser = argparse.ArgumentParser(description="Subset TEMPO data")
+def subset_files(filein, fileout, show_time = True):
+    
 
-# The file we want to subset
-parser.add_argument("-f", "--file", type=str, help="File to subset", required=True)
+    variables_to_keep = [
+        "geolocation/solar_zenith_angle",
+        "latitude",
+        "longitude",
+        "product/vertical_column_troposphere",
+        "product/vertical_column_troposphere_uncertainty",
+        "product/main_data_quality_flag",
+        "support_data/eff_cloud_fraction",
+        # "suppord_data/snow_ice_fraction",
+        "time",
+    ]
 
-# The output file name
-parser.add_argument("-o", "--output", type=str, help="Output file name", default="")
+    # print(" ======== Subsetting file ========= ")
+    # print(f"\t Input file: {filein}")
+    # print(f"\t Output file: {fileout}")
+    # print(" ================================== ")
+    logger.info(f"Subsetting file: {filein} to {fileout}")
 
-# delete original after subsetting
-parser.add_argument(
-    "-d", "--delete", action="store_true", help="Delete original file after subsetting"
-)
-
-# dry run flag
-parser.add_argument("-n", "--dry-run", action="store_true", help="Dry run")
-
-args = parser.parse_args()
-# terminate and show help if no arguments are given
-if len(sys.argv) == 1:
-    parser.print_help(sys.stderr)
-    sys.exit(1)
-
-filein = Path(args.file)
-
-if not filein.exists():
-    print(f"File {filein} does not exist")
-    sys.exit(1)
-
-# Options for the args.output
-# 1. If the output is not given, use the file name with the suffix '_subset'
-# 2. If it is a directory with a file name, use that file name and create the file in that directory
-# 3. If it is just a file name, use that file name
-# 4. If it is just a directory with no file name, use the file name with the suffix '_subset' in that directory
-
-if args.output == "":
-    fileout = filein.with_name(filein.stem + "_subset" + filein.suffix)
-elif Path(args.output).is_dir():
-    # create the directory if it does not exist
-    if not Path(args.output).exists():
-        Path(args.output).mkdir(parents=True, exist_ok=False)
-    fileout = Path(args.output) / filein.name
-else:
-    fileout = Path(args.output)
+    if fileout.exists():
+        logger.error(f"\nOutput file {fileout} already exists\n")
+        sys.exit(1)
 
 
-variables_to_keep = [
-    "geolocation/solar_zenith_angle",
-    "latitude",
-    "longitude",
-    "product/vertical_column_troposphere",
-    "product/vertical_column_troposphere_uncertainty",
-    "product/main_data_quality_flag",
-    "support_data/eff_cloud_fraction",
-    # "suppord_data/snow_ice_fraction",
-    "time",
-]
-
-print(" ======== Subsetting file ========= ")
-print(f"\t Input file: {filein}")
-print(f"\t Output file: {fileout}")
-print(" ================================== ")
-
-if fileout.exists():
-    print(f"\nOutput file {fileout} already exists\n")
-    sys.exit(1)
-
-
-if args.dry_run:
-    print("Dry run")
-else:
-    timer = Timer()
-    timer.start()
-    # adapted from https://stackoverflow.com/a/49592545/11594175
-    with nc.Dataset(filein) as src, nc.Dataset(fileout, "w") as dst:
-        dst.setncatts(src.__dict__)
-        for name, dimension in src.dimensions.items():
-            dst.createDimension(
-                name, (len(dimension) if not dimension.isunlimited() else None)
-            )
-        # need to copy groups and variables in the list. groups have a / in their name
-        for name, variable in src.variables.items():
-            if name in variables_to_keep:
-                chunksizes = variable.chunking()
-                if chunksizes == "contiguous":
-                    chunksizes = (1,)
-                x = dst.createVariable(
-                    name,
-                    variable.datatype,
-                    variable.dimensions,
-                    chunksizes=chunksizes,
-                    compression="zlib",
-                )
-                # x.set_var_chunk_cache(variable.get_var_chunk_cache())
-                dst[name].setncatts(src[name].__dict__)
-                dst[name][:] = src[name][:]
-
-        groups_to_keep = [g.split("/")[0] for g in variables_to_keep if "/" in g]
-        vars_to_keep = [v.split("/")[1] for v in variables_to_keep if "/" in v]
-        for group_name, group in src.groups.items():
-            if group_name in groups_to_keep:
-                dst.createGroup(group_name)
-                for name, variable in group.variables.items():
-                    if name in vars_to_keep:
-                        name = group_name + "/" + name
+    if args.dry_run:
+        logger.info("Dry run: Subsetting file")
+    else:
+        with TimedContext(use_timer = show_time) as timer:
+            # adapted from https://stackoverflow.com/a/49592545/11594175
+            with Dataset(filein) as src, Dataset(fileout, "w") as dst:
+                dst.setncatts(src.__dict__)
+                for name, dimension in src.dimensions.items():
+                    dst.createDimension(
+                        name, (len(dimension) if not dimension.isunlimited() else None)
+                    )
+                # need to copy groups and variables in the list. groups have a / in their name
+                for name, variable in src.variables.items():
+                    if name in variables_to_keep:
                         chunksizes = variable.chunking()
                         if chunksizes == "contiguous":
                             chunksizes = (1,)
@@ -150,13 +102,86 @@ else:
                         # x.set_var_chunk_cache(variable.get_var_chunk_cache())
                         dst[name].setncatts(src[name].__dict__)
                         dst[name][:] = src[name][:]
-    timer.stop()
 
-if args.delete:
-    if args.dry_run:
-        print("Dry run: Deleted", filein)
+                groups_to_keep = [g.split("/")[0] for g in variables_to_keep if "/" in g]
+                vars_to_keep = [v.split("/")[1] for v in variables_to_keep if "/" in v]
+                for group_name, group in src.groups.items():
+                    if group_name in groups_to_keep:
+                        dst.createGroup(group_name)
+                        for name, variable in group.variables.items():
+                            if name in vars_to_keep:
+                                name = group_name + "/" + name
+                                chunksizes = variable.chunking()
+                                if chunksizes == "contiguous":
+                                    chunksizes = (1,)
+                                x = dst.createVariable(
+                                    name,
+                                    variable.datatype,
+                                    variable.dimensions,
+                                    chunksizes=chunksizes,
+                                    compression="zlib",
+                                )
+                                # x.set_var_chunk_cache(variable.get_var_chunk_cache())
+                                dst[name].setncatts(src[name].__dict__)
+                                dst[name][:] = src[name][:]
+
+    if args.delete:
+        if args.dry_run:
+            logger.info(f"Dry run: Deleted {filein}")
+        else:
+            filein.unlink()
+            logger.debug(f"\nDeleted {filein}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Subset TEMPO data")
+
+    # The file we want to subset
+    parser.add_argument("-f", "--file", type=str, help="File to subset", required=True)
+
+    # The output file name
+    parser.add_argument("-o", "--output", type=str, help="Output file name", default="")
+
+    # delete original after subsetting
+    parser.add_argument(
+        "-d", "--delete", action="store_true", help="Delete original file after subsetting"
+    )
+    
+    # add debug flag
+    parser.add_argument("-v", "--debug", action="store_true", help="Verbose output")
+
+    # dry run flag
+    parser.add_argument("-n", "--dry-run", action="store_true", help="Dry run")
+
+    args = parser.parse_args()
+    # terminate and show help if no arguments are given
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+        
+    filein = Path(args.file)
+
+    if not filein.exists():
+        print(f"File {filein} does not exist")
+        sys.exit(1)
+    
+    # Options for the args.output
+    # 1. If the output is not given, use the file name with the suffix '_subset'
+    # 2. If it is a directory with a file name, use that file name and create the file in that directory
+    # 3. If it is just a file name, use that file name
+    # 4. If it is just a directory with no file name, use the file name with the suffix '_subset' in that directory
+
+    if args.output == "":
+        fileout = filein.with_name(filein.stem + "_subset" + filein.suffix)
+    elif Path(args.output).is_dir():
+        # create the directory if it does not exist
+        if not Path(args.output).exists():
+            Path(args.output).mkdir(parents=True, exist_ok=False)
+        fileout = Path(args.output) / filein.name
     else:
-        filein.unlink()
-        print(f"\nDeleted {filein}")
+        fileout = Path(args.output)
 
-print("\nDone\n")
+    if args.debug  or args.dry_run:
+        set_log_level(debug = True)
+    
+    subset_files(filein = filein, fileout = fileout)

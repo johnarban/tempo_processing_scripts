371816e4d50eb5238c2ad01d2b2f79fd6cd8431c
John Arban Lewis <johnarban@users.noreply.github.com>
2025-03-31 18:10:28 -0400

    stop using v01 to check

--- download_template.sh
+++ download_template.sh
@@ -25,14 +25,14 @@ exit_with_error() {
     echo
     echo $1
     echo
-    echo "https://data.asdc.earthdata.nasa.gov/asdc-prod-protected/TEMPO/TEMPO_NO2_L3_V01/2023.12.30/TEMPO_NO2_L3_V01_20231230T225114Z_S011.nc"
+    echo "https://data.asdc.earthdata.nasa.gov/asdc-prod-protected/TEMPO/TEMPO_NO2_L3_V03/2024.11.26/TEMPO_NO2_L3_V03_20241126T225208Z_S014.nc"
     echo
     exit 1
 }
 
 prompt_credentials
   detect_app_approval() {
-    approved=`curl -s -b "$cookiejar" -c "$cookiejar" -L --max-redirs 5 --netrc-file "$netrc" https://data.asdc.earthdata.nasa.gov/asdc-prod-protected/TEMPO/TEMPO_NO2_L3_V01/2023.12.30/TEMPO_NO2_L3_V01_20231230T225114Z_S011.nc -w '\n%{http_code}' | tail  -1`
+    approved=`curl -s -b "$cookiejar" -c "$cookiejar" -L --max-redirs 5 --netrc-file "$netrc" https://data.asdc.earthdata.nasa.gov/asdc-prod-protected/TEMPO/TEMPO_NO2_L3_V03/2024.11.26/TEMPO_NO2_L3_V03_20241126T225208Z_S014.nc -w '\n%{http_code}' | tail  -1`
     if [ "$approved" -ne "200" ] && [ "$approved" -ne "301" ] && [ "$approved" -ne "302" ]; then
         # User didn't approve the app. Direct users to approve the app in URS
         exit_with_error "Please ensure that you have authorized the remote application by visiting the link below "
@@ -41,7 +41,7 @@ prompt_credentials
 
 setup_auth_curl() {
     # Firstly, check if it require URS authentication
-    status=$(curl -s -z "$(date)" -w '\n%{http_code}' https://data.asdc.earthdata.nasa.gov/asdc-prod-protected/TEMPO/TEMPO_NO2_L3_V01/2023.12.30/TEMPO_NO2_L3_V01_20231230T225114Z_S011.nc | tail -1)
+    status=$(curl -s -z "$(date)" -w '\n%{http_code}' https://data.asdc.earthdata.nasa.gov/asdc-prod-protected/TEMPO/TEMPO_NO2_L3_V03/2024.11.26/TEMPO_NO2_L3_V03_20241126T225208Z_S014.nc | tail -1)
     if [[ "$status" -ne "200" && "$status" -ne "304" ]]; then
         # URS authentication is required. Now further check if the application/remote service is approved.
         detect_app_approval
